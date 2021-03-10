d072fa22c403e2fe9bedf3d658479920d33afce8
Shyam Sundar <shyam-11066@shyam-11066.csez.zohocorpin.com>
2021-03-11 01:42:33 +0530

    backend without event listing

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const fetch = require("node-fetch");
+const cors = require('cors');
 
-app.get("/authorization/:code/:clientid/:clientsecret", (req, res) => {
-  res.send("Hello World!");
+
+app.use(cors());
+
+app.get("/authorization/:code/:clientid/:clientsecret", async (req, res) => {
+  //fetch access token
+  let code = req.params.code;
+  let clientId = req.params.clientid;
+  let clientSecret = req.params.clientsecret;
+
+  let result = await fetch(
+    `https://accounts.zoho.com/oauth/v2/token?code=${code}&grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=http://127.0.0.1:3000/home&scope=AaaServer.profile.READ%2CAaaServer.profile.UPDATE`,
+    { method: "POST" }
+  ).then(resp => resp.json());
+  res.status(200).send(result.access_token);
 });
 
-app.get("/getCalendarList/:accesstoken", (req, res) => {
+app.get("/getCalendarList/:accesstoken", async (req, res) => {
   let accessToken = req.params.accesstoken;
   //fetch calendar list from api
+  let result = await fetch("https://calendar.zoho.com/api/v1/calendars", {
+    headers: { Autorization: `Zoho-oauthtoken ${accessToken}` },
+  })
+    .then((response) => response.JSON())
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+
+    res.status(200).send(result);
 });
 
 app.get("/getEventList/:accesstoken/:calendarid", (req, res) => {
